1111f9580e0a0ece95a05864ff4f53d37f12846e
WahidaAkhter <wahidacse280@gmail.com>
2022-03-25 22:52:56 +0600

    header added

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Toys Store</h2>
+            <h4>Choose four Toys</h4>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/Component/Shop/Shop.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Shop.css'
+
+const Shop = () => {
+
+
+
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                <h3>this is products</h3>
+            </div>
+
+            <div className="cart-container">
+                <h3>this is cart</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
